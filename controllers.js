278235348f323cee43edb93a82241cc4e3fe81e0
Michael <gurliaccio@gmail.com>
2017-05-30 19:34:07 -0400

    removed resource from controller

--- controllers.js
+++ controllers.js
@@ -17,14 +17,17 @@ weatherApp.controller('aboutController', ['$scope', function($scope) {
 										 
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$routeParams', 'cityService', 'weatherService', function($scope, $routeParams, cityService, weatherService) {
 	$scope.city = cityService.city;	
 	$scope.days = $routeParams.days || 2;
-	$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?id=4180439&appid=56acd9fb4a4f6138a4dd1055a7fde1fd", {
-		callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }}
-	);
-	$scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+	
+//	$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?id=4180439&appid=56acd9fb4a4f6138a4dd1055a7fde1fd", {
+//		callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }}
+//	);
+	$scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
+	
 	console.log($scope.weatherResult);
+	
 	$scope.convertToDate = function(dt) {
 		return new Date(dt * 1000);	
 	}
--- services.js
+++ services.js
@@ -1,3 +1,13 @@
 weatherApp.service('cityService', function() {
 	this.city = "Rome, IT";
-})
+});
+
+weatherApp.service('weatherService', ['$resource', function($resource) {
+	
+	this.GetWeather = function(city, days) {
+		var weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?id=4180439&appid=56acd9fb4a4f6138a4dd1055a7fde1fd", {
+		callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+		
+		return weatherAPI.get({ q: city, cnt: days });
+        };
+}]);
